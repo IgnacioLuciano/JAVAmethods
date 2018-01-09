643d287a55cf4da92365ecffcfbcb16a6b6e5bee
Unknown <iglumu@gmail.com>
2018-01-09 08:55:51 -0800

    Start

---
+++ JAVA-Methods-HandsOn/src/com/javamethods/MethodsHandsOn.java
@@ -0,0 +1,15 @@
+package com.javamethods;
+
+public class MethodsHandsOn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		inter doubleMe(){
+			
+			
+			
+		}
+	}
+
+}
